99979c51eddf9d3a5c73960d507c3ddf4415544c
BrendenSeifried <brendenseifried@gmail.com>
2022-05-12 13:35:57 -0700

    added button and array of items

--- src/services/Landing/Landing.jsx
+++ src/services/Landing/Landing.jsx
@@ -1,24 +1,42 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Landing() {
-  const [item, setItem] = useState('test');
+  const [allItems, setAllItems] = useState([]);
+  const [item, setItem] = useState('');
   const [load, setLoad] = useState(true);
 
   useEffect(() => {
     const placeItems = async () => {
-      // setItem('');
+      const data = item;
+      setAllItems(data);
       setLoad(false);
     };
     placeItems();
   }, []);
 
+  const submitItem = async () => {
+    const data = item;
+    setAllItems((prevState) => [...prevState, data]);
+    setItem('');
+  };
+
   if (load) return <h1>Loading</h1>;
 
   return (
-    <div>
-      Landing
-      <input />
+    <>
+      <div>
+        Landing
+        <label>
+          Enter a grocery item:
+          <input
+            type="text"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+        </label>
+        <button onClick={submitItem}>Add</button>
+      </div>
       <h1>{item}</h1>
-    </div>
+    </>
   );
 }
